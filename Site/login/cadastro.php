ee4bf9c623b8db19e2da06b6d65571b5c7477f85
MikaelCastro <mikaelcastrosouza@hotmail.com>
2020-12-01 10:42:58 -0300

    Site final completo 3.0

--- Site/login/cadastro.php
+++ Site/login/cadastro.php
@@ -61,7 +61,7 @@ session_start();
        <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="../Contato.php"><h6><FONT COLOR="white">ENTRE EM CONTATO</FONT></h4></a>
+            <a class="nav-link" href="../contato/index.php"><h6><FONT COLOR="white">ENTRE EM CONTATO</FONT></h4></a>
           </li>
 
           <div class="collapse navbar-collapse" id="navbarResponsive">
